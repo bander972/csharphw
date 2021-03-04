5a44ab720b9fefe4cbfe8617f5626f1ba49632bb
bander972 <1628580708@qq.com>
2021-03-04 22:27:10 +0800

    Initial commit

---
+++ HomeWork1/CaculatorConsole/CaculatorConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CaculatorConsole
+{
+    class caculator
+    {
+        public static void GetValue(String func, double n1, double n2)
+        {
+
+            double res = 0;
+            try
+            {
+                switch (func)
+                {
+                    case "+":
+                        res = n1 + n2;
+                        break;
+                    case "-":
+                        res = n1 - n2;
+                        break;
+                    case "*":
+                        res = n1 * n2;
+                        break;
+                    case "/":
+                        if (n2 == 0) { Console.WriteLine("Invalid number!"); }
+                        res = n1 / n2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid op! input your op again!");
+                        String s = Console.ReadLine();
+                        GetValue(s, n1, n2);
+                        break;
+                }
+                Console.WriteLine("= " + res);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("wrong input");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input the first number,the op and the second number and the op in order(use space key):");
+            String[] s = Console.ReadLine().Split(' ');
+            double num1 = double.Parse(s[0]);
+            double num2 = double.Parse(s[2]);
+            string func = s[1];
+            GetValue(func, num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ HomeWork1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<char> inputstr = new List<char>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        {
+            inputstr.Add('-');
+            txtbox_1.Text.Append<char>('-');
+
+        }
+
+    
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            inputstr.Add('+');
+            txtbox_1.Text.Append<char>('+');
+        }
+
+        private void btn_equals_Click(object sender, RoutedEventArgs e)
+        {
+            inputstr.Add('=');
+            txtbox_1.Text.Append<char>('=');
+
+        }
+
+        private void btn_9_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
