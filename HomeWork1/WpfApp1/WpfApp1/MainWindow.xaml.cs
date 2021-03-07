9d2235815284589cbbe067033ea36cb37a07bbfd
bander972 <1628580708@qq.com>
2021-03-07 19:05:10 +0800

    New Update Success

--- HomeWork1/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ HomeWork1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -16,143 +16,152 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-
+   
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        const double pi = 3.14;
         double num1, num2=0.0;
+        String op="";
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public String Value(String s) {
-            String a=s;
-            String[] b= a.Split('+', '-', '*', '/', '=');
-            String sum=" ";
-            for (int i = 0; i < b.Length - 1; i++)
-            {
-                switch (s[1])
-                {
-                    case '+':
-                        num1 = Convert.ToDouble(b[0]); num2 = Convert.ToDouble(b[1]);
-                        sum = (num1 + num2).ToString();
-                        break;
-                    case '-':
-                        Convert.ToDouble(b[0]); num2 = Convert.ToDouble(b[1]);
-                        sum = (num1 - num2).ToString();
-                        break;
-                    case '*':
-                        Convert.ToDouble(b[0]); num2 = Convert.ToDouble(b[1]);
-                        sum = (num1 * num2).ToString();
-                        break;
-                    case '/':
-                        Convert.ToDouble(b[0]); num2 = Convert.ToDouble(b[1]);
-                        if (num2 == 0)
-                        {
-                            Console.WriteLine("Invalid divide number!");
-                            return null;
-                        }
-                        else sum = (num1 / num2).ToString();
-                        break;
-                    default:
-                        Console.WriteLine("Invalid op");
-                        break;
-
-
-                }
-            }
-           
-                return sum;
-           
-         
-            
-            }
             
         private void btn_minus_Click(object sender, RoutedEventArgs e)
-        {  
-            txtbox_1.Text += btn_0.Content;
-            
+        {
+            num1 = double.Parse(txtbox_2.Text);
+            txtbox_1.Text += btn_minus.Content;
+            txtbox_2.Text = "";
+            op = "-";
+
         }
 
         private void btn_plus_Click(object sender, RoutedEventArgs e)
-        {   
+        {
+            num1 = double.Parse(txtbox_2.Text);
             txtbox_1.Text += btn_plus.Content;
+            txtbox_2.Text = "";
+            op = "+";
         }
 
         private void btn_equal_Click(object sender, RoutedEventArgs e)
         {
-            txtbox_1.Text = txtbox_1.Text + btn_equal.Content;
-            txtbox_1.Text = Value(txtbox_1.Text);
+            num2 = double.Parse(txtbox_2.Text);
+            switch (op)
+            {
+                case "+": txtbox_2.Text = (num1 + num2).ToString();
+                    txtbox_1.Text += ("=" + txtbox_2.Text);
+                    break;
+                case "-":
+                    txtbox_2.Text = (num1 - num2).ToString();
+                    txtbox_1.Text += ("=" + txtbox_2.Text);
+                    break;
+                case "*":
+                    txtbox_2.Text = (num1 * num2).ToString();
+                    txtbox_1.Text += ("=" + txtbox_2.Text);
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        Console.WriteLine("MathException error");
+                        return;
+                    }
+                    else
+                    {
+                        txtbox_2.Text = (num1 / num2).ToString();
+                        txtbox_1.Text += ("=" + txtbox_2.Text);
+                    }
+                    break;
+                default: Console.WriteLine("Invalid op");
+                    return;
+            }
         }
 
         private void btn_9_Click(object sender, RoutedEventArgs e)
         { 
             txtbox_1.Text += btn_9.Content;
+            txtbox_2.Text += btn_9.Content;
         }
         private void btn_8_Click(object sender, RoutedEventArgs e)
         {  
             txtbox_1.Text += btn_8.Content;
+            txtbox_2.Text += btn_8.Content;
         }
         private void btn_7_Click(object sender, RoutedEventArgs e)
         {   
             txtbox_1.Text += btn_7.Content;
+            txtbox_2.Text += btn_7.Content;
         }
         private void btn_6_Click(object sender, RoutedEventArgs e)
         {           
             txtbox_1.Text += btn_6.Content;
+            txtbox_2.Text += btn_6.Content;
         }
 
         private void btn_5_Click(object sender, RoutedEventArgs e)
         {            
             txtbox_1.Text += btn_5.Content;
+            txtbox_2.Text += btn_5.Content;
         }
 
         private void btn_4_Click(object sender, RoutedEventArgs e)
         {        
             txtbox_1.Text += btn_4.Content;
+            txtbox_2.Text += btn_4.Content;
         }
         private void btn_3_Click(object sender, RoutedEventArgs e)
         {           
             txtbox_1.Text+=btn_3.Content;
+            txtbox_2.Text += btn_3.Content;
         }
 
         private void btn_2_Click(object sender, RoutedEventArgs e)
         {           
             txtbox_1.Text += btn_2.Content;
+            txtbox_2.Text += btn_2.Content;
         }
 
         private void btn_1_Click(object sender, RoutedEventArgs e)
         {          
             txtbox_1.Text += btn_1.Content;
+            txtbox_2.Text += btn_1.Content;
         }
 
         private void btn_0_Click(object sender, RoutedEventArgs e)
         {           
             txtbox_1.Text += btn_0.Content;
+            txtbox_2.Text += btn_0.Content;
         }
 
         private void btn_dot_Click(object sender, RoutedEventArgs e)
         {           
             txtbox_1.Text += btn_dot.Content;
+            txtbox_2.Text += btn_dot.Content;
         }
 
-        private void btn_pi_Click(object sender, RoutedEventArgs e)
-        {            
-            txtbox_1.Text += btn_pi.Content;
+        private void btn_ac_Click(object sender, RoutedEventArgs e)
+        {
+            txtbox_1.Text = "";
+            txtbox_2.Text = "";
         }
 
         private void btn_multiple_Click(object sender, RoutedEventArgs e)
-        {            
+        {
+            num1 = double.Parse(txtbox_2.Text);
             txtbox_1.Text += btn_multiple.Content;
+            txtbox_2.Text = "";
+            op = "*";
         }
 
         private void btn_divide_Click(object sender, RoutedEventArgs e)
-        {           
+        {
+            num1 = double.Parse(txtbox_2.Text);
             txtbox_1.Text += btn_divide.Content;
+            txtbox_2.Text = "";
+            op = "/";
         }
 
         private void txtbox_1_TextChanged(object sender, TextChangedEventArgs e)
